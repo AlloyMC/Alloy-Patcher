c7a75a4b7a525a0c31280a9f1c62843bf466f1b0
valoeghese <valoeghesemods@gmail.com>
2020-07-24 20:16:43 +1200

    dir handling

--- src/main/java/io/github/alloymc/patcher/Patcher.java
+++ src/main/java/io/github/alloymc/patcher/Patcher.java
@@ -1,9 +1,19 @@
 package io.github.alloymc.patcher;
 
+import java.io.File;
+
 public class Patcher implements Runnable {
 	@Override
 	public void run() {
-		
+		File inputDir = new File("input");
+		File[] modJars;
+
+		if (!inputDir.mkdir() && (modJars = inputDir.listFiles()).length > 0) {
+			File outputDir = new File("output");
+			outputDir.mkdir();
+		} else {
+			LOGGER.log("Unable to patch, as no mods are in the input directory!");
+		}
 	}
 
 	private static final Logger LOGGER = new Logger("Patcher");
